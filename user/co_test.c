249a4cc7e961de6758f2e34bc5486448d7cf966a
barzilie <noambarzilie@gmail.com>
2026-04-28 10:32:16 +0000

    updated co_test for error test case

--- user/co_test.c
+++ user/co_test.c
@@ -4,6 +4,33 @@
 
 int main()
 {
+
+  printf("error test case 1: PID not exist\n");
+  printf("CALLING: co_yield(1234567, 1)\n");
+  printf("co yeild returnd: %d\n",co_yield(1234567, 1));
+  printf("------------------------------------------\n");
+
+  printf("error test case 2: PID killed\n");
+  int killed_pid2 = fork();
+  //kill child
+  if(killed_pid2 == 0){
+    exit(0);
+  }
+  else{
+    wait(0);
+  }
+  printf("CALLING: co_yield(killed_pid2, 1)\n");
+  printf("co yeild returnd: %d\n",co_yield(killed_pid2, 1));
+  printf("------------------------------------------\n");
+
+  printf("error test case 3: self co_yeild\n");
+  printf("CALLING: co_yield(getpid(), 1)\n");
+  printf("co yeild returnd: %d\n",co_yield(getpid(), 1));
+  printf("------------------------------------------\n");
+
+  printf("base succesfull test case: co_yeild\n");
+  printf("------------------------------------------\n");
+
   int pid1 = getpid(); // Parent PID
   int pid2 = fork();   // Child PID
   if (pid2 == 0)
